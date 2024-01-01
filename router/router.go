892d8f35f53ce5b06e5fead7525b9ce8ae1e96c6
vitorsouzaemgo <vitorsouzaemgo@gmail.com>
2024-01-01 10:01:19 -0300

    feat: add routes logic

--- router/router.go
+++ router/router.go
@@ -6,11 +6,9 @@ import (
 
 // Função exportável "I maíusculo"
 func Initialize() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run(":8080") // listen and serve on 0.0.0.0:8080
+	router := gin.Default()
+
+	initializeRoutes(router)
+
+	router.Run(":8080") // listen and serve on 0.0.0.0:8080
 }
